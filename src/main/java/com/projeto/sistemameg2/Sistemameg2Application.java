1e9af027561238cd009983efc0262ba8f5ab6e8e
juansporto <juanporto.ti@gmail.com>
2025-05-07 22:02:34 -0300

    comecando o projeto da loja MeG kids para meio academicos.

---
+++ src/main/java/com/projeto/sistemameg2/Sistemameg2Application.java
@@ -0,0 +1,13 @@
+package com.projeto.sistemameg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sistemameg2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sistemameg2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/sistemameg2/Sistemameg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.sistemameg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sistemameg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
